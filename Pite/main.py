2b718978c17cc547b4e62964d53afe48104b5638
Boti <sumegi.arpad.botond@students.jedlik.eu>
2026-05-06 11:41:36 +0200

    Add files via upload

---
+++ Pite/main.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+# pyright: reportOptionalMemberAccess=false
+# pyright: reportMissingImports=false
+
+from ev3dev2.motor import MediumMotor, Motor, OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D,  SpeedPercent, MoveTank
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
+from ev3dev2.sensor.lego import ColorSensor, GyroSensor, InfraredSensor, UltrasonicSensor
+from ev3dev2.led import Leds
+from ev3dev2.display import Display
+from ev3dev2.sound import Sound
+from ev3dev2.button import Button
+from ev3dev2.power import PowerSupply
+from ev3dev2.console import Console
+from wroRobot import WroRobot
+from task import Task
+from time import time, sleep
+import ev3dev2.fonts as fonts
+import sys
+button = Button()
+leds = Leds()
+sound = Sound()
+def error(e):
+    sound.speak("ERROR")
+    leds.set_color('LEFT', 'RED')
+    leds.set_color('RIGHT', 'RED')
+    while not button.enter:
+        pass
+
+    f = open('error.txt', 'w')
+    print("Error: \n\t{}\n\n\n\n".format(e), file=sys.stderr)
+    f.write("Error: \n\t{}".format(e))
+    Not_working = []
+    Motor_ports = [OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D]
+    for port in Motor_ports:
+        try:
+            motor = MediumMotor(port)
+            motor.on_for_seconds(speed=50, seconds=0.5)
+            motor.on_to_position(speed=100, position=0)
+        except:
+            Not_working.append(port)
+    Sensor_ports = [INPUT_1, INPUT_2, INPUT_3, INPUT_4]
+    for port in Sensor_ports:
+        try:
+            if port == INPUT_1:
+                s = GyroSensor(port)
+                print("Test angle: {}".format(s.angle), file=sys.stderr)
+                f.write("Test angle: {}\n".format(s.angle))
+            else:
+                s = ColorSensor(port)
+                print("Test reflected: {}".format(s.reflected_light_intensity), file=sys.stderr)
+                f.write("Test reflected: {}\n".format(s.reflected_light_intensity))
+        except:
+            Not_working.append(port)
+    print("Ports that are not connected: {}".format(Not_working), file=sys.stderr)
+    f.write("Ports that are not connected: {}\n".format(Not_working))
+    f.close()
+
+
+try:
+    robot = WroRobot()
+    # robot.calibrate()
+    task = Task(robot)
+    try:
+        leds.set_color('LEFT', 'AMBER')
+        leds.set_color('RIGHT', 'AMBER')
+        robot.starting()
+        leds.set_color('LEFT', 'AMBER')
+        leds.set_color('RIGHT', 'AMBER')
+        start_time = time()
+        task.rohadjmeg()
+        task.masodik()
+
+        robot.log("Time: {:.4f}s".format((time() - start_time)))
+    except Exception as f:
+        robot.log(f)
+    finally:
+        robot.stop()
+        robot.gyroadatok.close()
+        exit()
+except Exception as e:
+    error(e)
+
---
+++ Pite/task.py
@@ -0,0 +1,148 @@
+# pyright: reportOptionalMemberAccess=false
+# pyright: reportMissingImports=false
+from ev3dev2.motor import MediumMotor, OUTPUT_A, OUTPUT_D, SpeedPercent, Motor
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
+from ev3dev2.sensor.lego import ColorSensor, TouchSensor
+from htcolor import HTColor
+from wroRobot import WroRobot
+from time import sleep, time
+import sys
+import os
+from ev3dev2.button import Button
+from ev3dev2.led import Leds
+import threading
+
+os.system('setfont Lat15-TerminusBold32x16')
+
+class Task:
+    def __init__(self, robot: WroRobot):
+        # password: "maker"
+        
+        self.robot = robot
+        self.leds = Leds()
+        self.button = Button()
+        self.grabber = MediumMotor(address=OUTPUT_A)
+        self.emelo = MediumMotor(address=OUTPUT_D)
+        self.emelo.polarity = Motor.POLARITY_INVERSED
+        # pozitív degrees -> Befele
+        # pozitív degrees -> Felfele
+
+
+    def e_g_nulla(self, var = True, speed = 100):
+        if var == True:
+            self.grabber.on_to_position(speed=speed, position=0, block=True)
+            self.emelo.on_to_position(speed=speed, position=0, block=True)
+        else:
+            self.grabber.on_to_position(speed=speed, position=0, block=False)
+            self.emelo.on_to_position(speed=speed, position=0, block=False)
+
+    def vege(self):
+        self.robot.gyroadatok.close()
+        self.grabber.on_to_position(speed=100, position=0, block=False)
+        self.emelo.on_to_position(speed=100, position=0, block=True)
+        exit()
+
+    def startup(self):
+        self.robot.forwardCmWithGyro(speed=500, angle=0, distance=5)
+        self.robot.forwardCm(speed=500, distance=-5)
+        self.grabber.on_for_degrees(speed=100, degrees=400, block=False)
+        self.emelo.on_for_degrees(speed=100, degrees=300, block=True)
+        self.e_g_nulla()
+
+
+
+
+
+    def rohadjmeg(self):
+        self.grabber.stop_action = 'hold'
+        self.emelo.stop_action = 'hold'
+        self.grabber.position = 0
+        self.emelo.position = 0
+
+        # előre megy a 1. 3 kockához és megfogja
+        self.robot.forwardCmWithGyro(speed=800, distance=12, angle=0)
+        self.grabber.on_to_position(speed=40, position=100, block=True)
+        # megfogja a kockákat a belsejébe
+        self.robot.forwardCmWithGyro(speed=800, distance=-19, angle=0)
+        self.grabber.on_to_position(speed=40, position=0)
+        self.robot.forwardCmWithGyro(speed=800, distance=9, angle=0)
+        self.grabber.on_to_position(speed=100, position=110)
+        sleep(0.1)
+        self.grabber.on_to_position(speed=100, position=0)
+        self.grabber.on_to_position(speed=100, position=110)
+        # elviszi a kockákat a helyére
+        self.robot.turnToGyroAngle(speed=200, angle=90)
+        self.robot.forwardCmWithGyro(speed=800, distance=43, angle=90)
+        self.robot.turnToGyroAngle(speed=200, angle=0)
+        # feketere egyenesedes 0 fokon
+        self.robot.alignToBlack(speed=800, blackThreshold=7)
+        self.robot.forwardCmWithGyro(speed=800, distance=38, angle=0)
+        # feketere egyenesedes 0 fokon
+        self.robot.alignToBlack(speed=200)
+        self.robot.forwardCmWithGyro(speed=800, distance=4, angle=0)
+        # elfordul a mozaik felé
+        self.robot.turnToGyroAngle(speed=200, angle=90)
+        self.grabber.on_to_position(speed=100, position=300, block=True)
+        self.emelo.on_to_position(speed=45, position=250)
+        self.robot.forwardCmWithGyro(speed=800, distance=20, angle=90)
+        self.robot.alignToBlack(speed=200)
+        # 90 fokra fordul a mozaik előtt, majd beviszi a 1*3 kockát
+        self.robot.turnToGyroAngle(speed=200, angle=90)
+        # self.robot.buttonPress()
+        self.robot.forwardCmWithGyro(speed=500, distance=29.5, angle=90)
+        self.grabber.stop()
+        self.grabber.on_to_position(speed=100, position=0)
+        sleep(0.3)
+        # hárta megy a fekete vonalig
+        self.robot.forwardCmWithGyro(speed=500, distance=-65, angle=90)
+        self.robot.alignToBlack(speed=-200)
+        self.e_g_nulla()
+
+    def masodik(self):
+        # elmegy a 2. 1*3 kockáért
+        self.robot.turnToGyroAngle(speed=200, angle=180)
+        self.robot.forwardCmWithGyro(speed=800, distance=32, angle=180)
+        self.robot.alignToBlack(speed=200)
+        self.robot.forwardCmWithGyro(speed=800, distance=4, angle=180)
+        self.robot.turnToGyroAngle(speed=200, angle=90)
+        self.robot.forwardCmWithGyro(speed=800, distance=-25.5, angle=90)
+        self.robot.turnToGyroAngle(speed=200, angle=0)
+        # felveszi az 1*3 kockát
+        self.robot.forwardCmWithGyro(speed=800, distance=3, angle=0)
+        self.grabber.on_to_position(speed=100, position=100)
+        # self.robot.buttonPress()
+        self.robot.forwardCmWithGyro(speed=800, distance=-25, angle=0)
+        self.grabber.on_to_position(speed=100, position=0)
+        self.robot.forwardCmWithGyro(speed=800, distance=9, angle=0)
+        self.grabber.on_to_position(speed=100, position=110)
+        sleep(0.1)
+        self.grabber.on_to_position(speed=100, position=0)
+        self.grabber.on_to_position(speed=100, position=110)
+        self.robot.turnToGyroAngle(speed=200, angle=90)
+        # elviszi a kockákat
+        self.robot.forwardCmWithGyro(speed=800, distance=41, angle=90)
+        self.robot.turnToGyroAngle(speed=200, angle=0)
+        self.robot.alignToBlack(speed=500, blackThreshold=7)
+        self.robot.forwardCmWithGyro(speed=800, distance=38, angle=0)
+        # vonalra áll 0 fokon
+        self.robot.alignToBlack(speed=200)
+        self.robot.forwardCmWithGyro(speed=800, distance=4, angle=0)
+        self.robot.turnToGyroAngle(speed=200, angle=90)
+        self.grabber.on_to_position(speed=100, position=300, block=True)
+        self.emelo.on_to_position(speed=45, position=500)
+        self.robot.forwardCmWithGyro(speed=800, distance=20, angle=90)
+        # vonalra áll 0 fokon
+        self.robot.alignToBlack(speed=200)
+        self.robot.turnToGyroAngle(speed=200, angle=90)
+        # self.robot.buttonPress()
+        # beviszi a 2. 1*3 kockát
+        self.robot.forwardCmWithGyro(speed=800, distance=22, angle=90)
+        self.grabber.stop()
+        self.emelo.on_to_position(speed=100, position=100)
+        self.grabber.on_to_position(speed=100, position=0)
+        self.emelo.on_to_position(speed=100, position=500)
+        sleep(0.3)
+        self.robot.forwardCmWithGyro(speed=800, distance=-60, angle=90)
+        self.robot.alignToBlack(speed=-200)
+        self.e_g_nulla()
+        
